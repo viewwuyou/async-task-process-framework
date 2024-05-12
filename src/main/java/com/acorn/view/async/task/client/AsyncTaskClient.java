c139f45d9ea9ea8c770f70430b85140f73baeba0
view <viewwuyou@gmail.com>
2024-05-12 23:07:20 +0800

    init project

---
+++ src/main/java/com/acorn/view/async/task/client/AsyncTaskClient.java
@@ -0,0 +1,9 @@
+package com.acorn.view.async.task.client;
+
+/**
+ * 异步任务处理Client
+ * @author view
+ * @date 2024/5/12 23:06
+ */
+public class AsyncTaskClient {
+}
---
+++ src/main/java/com/acorn/view/async/task/core/model/Task.java
@@ -0,0 +1,8 @@
+package com.acorn.view.async.task.core.model;
+
+/**
+ * @author view
+ * @date 2024/5/12 23:06
+ */
+public class Task {
+}
